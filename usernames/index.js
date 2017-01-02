e14e549d8d48daca9aa2ad1bad5dba52af51846d
bengreenier <ben@bengreenier.com>
2017-01-02 13:48:40 -0500

    autoparse

--- usernames/index.js
+++ usernames/index.js
@@ -1,6 +1,6 @@
 // https://webtask.io/docs/model
 module.exports = function (context, req) {
-    var body = JSON.parse(req.body || "{}")
+    var body = req.body || {}
     var cb = context.done
     var version = body.version || "1.0"
 
